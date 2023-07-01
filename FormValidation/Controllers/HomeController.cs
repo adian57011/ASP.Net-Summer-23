617978f65d74fc94dc464e8afcc5a038f6521a81
adian57011 <adian57011@gmail.com>
2023-07-01 20:24:12 +0600

    2nd project of the daygit add .

---
+++ FormValidation/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using FormValidation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+       [HttpGet]
+       public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(UserDto user)
+        {
+            if(ModelState.IsValid)
+            {
+               return RedirectToAction("Home");
+            }
+            return View(user);
+        }
+
+        public ActionResult Home(UserDto user)
+        {
+            return View(user);
+        }
+    }
+}
---
+++ FormValidation/Models/UserDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FormValidation.Models
+{
+    public class UserDto
+    {
+        [Required(ErrorMessage ="Please Enter Name")]
+        [StringLength(5,ErrorMessage ="Maximum Character limit reached")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Please enter your age")]
+        public string Age { get; set; }
+        [Required(ErrorMessage ="Please fill your gender")]
+        public string Gender { get; set; }
+        [Required(ErrorMessage ="Please enter your email")]
+        [EmailAddress(ErrorMessage ="Email is not valid")]
+        public string Email { get; set; }
+        [Required(ErrorMessage ="Please fill up your contact information")]
+        
+        public string Contact { get; set; }
+    }
+}
